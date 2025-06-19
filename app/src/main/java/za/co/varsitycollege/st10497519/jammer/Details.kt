dbe223bd93588c5613b4232b8a76a07269c6a86f
ST10497519 <ST10497519@vcconnect.edu.za>
2025-06-19 10:55:30 +0200

    Added coding for 2nd screen

--- app/src/main/java/za/co/varsitycollege/st10497519/jammer/Details.kt
+++ app/src/main/java/za/co/varsitycollege/st10497519/jammer/Details.kt
@@ -1,20 +1,50 @@
 package za.co.varsitycollege.st10497519.jammer
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.RatingBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Details : AppCompatActivity() {
+
+    var songListView = findViewById<ListView>(R.id.songListView)
+    var songList = ArrayList<String>()
+    var adapter: ArrayAdapter<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        songListView = findViewById<ListView>(R.id.songListView)
+        songList = ArrayList()
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, songList)
+        songListView.adapter = adapter
+
+        val songCount = 4
+        for (i in 1..songCount) {
+            val title = intent.getStringExtra("title$i")
+            val artist = intent.getStringExtra("artist$i")
+            val rating = intent.getFloatExtra("rating$i", 0.0f)
+            val comment = intent.getStringExtra("comment$i")
+
+            songList.add("Title: $title")
+            songList.add("Artist: $artist")
+            songList.add("Rating: $rating")
+            songList.add("Comment: $comment")
+            songList.add("")
+
         }
-    }
-}
+
+        val displayButton = findViewById<Button>(R.id.displayButton)
+        val averageButton = findViewById<Button>(R.id.averageButton)
+        val returnButton = findViewById<Button>(R.id.returnButton)
+
+        }
+        }
---
+++ app/src/main/java/za/co/varsitycollege/st10497519/jammer/Song.kt
@@ -0,0 +1,4 @@
+package za.co.varsitycollege.st10497519.jammer
+
+class Song(val title: String, val artist: String, val rating: Float, val comment: String){
+}
